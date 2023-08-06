c0546b2d9a99b51567d601293df75d8343ec8e85
mossa <martin.ossa.8@gmail.com>
2023-08-06 18:06:09 +0200

    Fix navbar styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import './styles/Navbar.css'; 
 
 const Navbar = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const handleNavbarToggle = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink exact to="/" activeClassName="active">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects" activeClassName="active">Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/skills" activeClassName="active">Skills</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className={`hamburger ${isNavbarOpen ? 'open' : ''}`} onClick={handleNavbarToggle}>
+        <div className={`hamburger-line ${isNavbarOpen ? 'open' : ''}`}></div>
+        <div className={`hamburger-line ${isNavbarOpen ? 'open' : ''}`}></div>
+        <div className={`hamburger-line ${isNavbarOpen ? 'open' : ''}`}></div>
+      </div>
+      <div className="navbar-links" style={{ display: isNavbarOpen ? 'flex' : 'none' }}>
+      {/* <div className={`navbar-links ${isNavbarOpen ? 'open' : ''}`}> */}
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="active" onClick={handleNavbarToggle}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects" activeClassName="active" onClick={handleNavbarToggle}>
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/skills" activeClassName="active" onClick={handleNavbarToggle}>
+                Skills
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" activeClassName="active" onClick={handleNavbarToggle}>
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+      </div>
     </nav>
   );
 };
