53999887899924933450a1e95c2b6ee5e1285a9d
mossa <martin.ossa.8@gmail.com>
2023-08-07 00:15:08 +0200

    Add animated logo to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo'
 
 const Intro = () => {
+
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center gap-3' style={{ height: '100vh', width: '100vw'}}>
-      <h1>Hey, I'm Martin.</h1>
-      <h4 className='text-center w-75'>I'm a web developer. I like to create fun and interactive SPAs.</h4>
-      {/* <button className='w-25'>Download my resume</button> */}
-      <a href='/projects' className='text-center w-50 text-dark'>Check my projects</a>
+    <div className='d-flex flex-column justify-content-center align-items-center' style={{ width: '100vw'}}>
+      <div>
+        <Logo />
+      </div>
+      <div className='d-flex flex-column justify-content-center align-items-center gap-3' style={{ height: '45vh', backgroundColor: '#3335'}}>
+        <h1>Hey, I'm Martin.</h1>
+        <h4 className='text-center w-75'>I'm a web developer. I like to create fun and interactive SPAs.</h4>
+        {/* <button className='w-25'>Download my resume</button> */}
+        <NavLink to="/projects" className="btn btn-dark mt-4">
+          See my projects
+        </NavLink>
+      </div>
     </div>
   );
 };
