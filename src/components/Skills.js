bc910fa0369f9f36ea9a0824e32b3aa72343a425
mossa <martin.ossa.8@gmail.com>
2023-08-09 20:24:41 +0200

    Update skills component styles

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import './styles/Skills.css';
+
+import javascript from '../assets/skills/JavaScript_Logo.png'
+import html from '../assets/skills/HTML5_logo.png'
+import react from '../assets/skills/react-logo.png'
+import bootstrap from '../assets/skills/Bootstrap_Logo.png'
+import rails from '../assets/skills/rails-logo.png'
+import python from '../assets/skills/python-logo.png'
 
 const Skills = () => {
-  const images = ['javascript_logo.png', 'react_logo.png', 'ruby_logo.png', 'tailwind_logo.png'];
+  const [isFlipped, setIsFlipped] = useState([false, false]);
+
+  const handleCardClick = (index) => {
+    const newFlippedStates = [...isFlipped];
+    newFlippedStates[index] = !newFlippedStates[index];
+    setIsFlipped(newFlippedStates);
+  };
 
   return (
-    <div className='bg-primary d-flex flex-column justify-content-center align-items-center gap-3' style={{ height: '100vh', width: '100vw'}}>
-      <h2>Skills</h2>
-      <div className='row justify-content-center'>
-        {images.map((image, index) => (
-          <div key={index} className='col-md-6 col-lg-3'>
-            <img src={image} alt={`Skill ${index + 1}`} className='img-fluid' />
+    <div className='d-flex flex-column justify-content-center align-items-center gap-4 my-5' style={{height: '75vh'}}>
+      <div className={`flip-card ${isFlipped[0] ? 'flipped' : ''}`} onClick={() => handleCardClick(0)}>
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <h2>Hard Skills</h2>
           </div>
-        ))}
+          <div className="flip-card-back">
+            <ul className='skills-list'>
+              <li><img src={javascript} alt='javascript' className='img-fluid'></img></li>
+              <li><img src={html} alt='html' className='img-fluid'></img></li>
+              <li><img src={react} alt='react' className='img-fluid'></img></li>
+              <li><img src={bootstrap} alt='bootstrap' className='img-fluid'></img></li>
+              <li><img src={rails} alt='rails' className='img-fluid'></img></li>
+              <li><img src={python} alt='python' className='img-fluid'></img></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={`flip-card ${isFlipped[1] ? 'flipped' : ''}`} onClick={() => handleCardClick(1)}>
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <h2>Soft Skills</h2>
+          </div>
+          <div className="flip-card-back">
+            <ul className='skills-list'>
+              <li className='soft-skills green'>Remote Working</li>
+              <li className='soft-skills blue'>Pair programming</li>
+              <li className='soft-skills blue'>Communication skills</li>
+              <li className='soft-skills green'>Professional</li>
+              <li className='soft-skills green'>Continuous Learning</li>
+              <li className='soft-skills blue'>Challenge enthusiast</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
