4999187d95c13adec49c1bab921736eca7f1fa4a
mossa <martin.ossa.8@gmail.com>
2023-08-09 20:25:51 +0200

    Fix intro background color

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
       <div>
         <Logo />
       </div>
-      <div className='d-flex flex-column justify-content-center align-items-center gap-3' style={{ height: '45vh', backgroundColor: '#3335'}}>
+      <div className='d-flex flex-column justify-content-center align-items-center gap-3' style={{ height: '45vh', backgroundColor: '#1115'}}>
         <h1>Hey, I'm Martin.</h1>
         <h4 className='text-center w-75'>I'm a web developer. I like to create fun and interactive SPAs.</h4>
         {/* <button className='w-25'>Download my resume</button> */}
