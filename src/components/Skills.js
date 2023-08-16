2bb24047a84b805b8aeeee62687e13eb46500e52
mossa <martin.ossa.8@gmail.com>
2023-08-16 11:24:21 +0200

    Update skills styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,11 +18,46 @@ const Skills = () => {
   };
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center gap-2 border border-dark' style={{height: '94vh', backgroundColor: '#dff706bb', paddingTop: '5vh'}}>
+    <section>
+      <div class="main-content w-100 h-100">
+        <div class="palette__wrapper">
+          <div class="palette palette--one">
+            <div class="palette__cover" style={{backgroundColor: '#5E503F'}}>
+              <div class="palette__cover__border"><span style={{backgroundColor: '#5E503F'}}>Hard Skills </span></div>
+            </div>
+            <div class="palette__cover__top"></div>
+            <div class="palette__base">
+              <div class="palette__shade"></div>
+              <div class="palette__shade"></div>
+              <div class="palette__shade"></div>
+              <div class="palette__shade"></div>
+              <div class="palette__shade"></div>
+              <div class="palette__shade"></div>
+            </div>
+          </div>
+        </div>
+        <div class="palette__wrapper">
+          <div class="palette palette--two">
+            <div class="palette__cover" style={{backgroundColor: '#22333B'}}> 
+              <div class="palette__cover__border"><span style={{backgroundColor: '#22333B'}}>Soft Skills</span></div>
+            </div>
+            <div class="palette__cover__top"></div>
+            <div class="palette__base text-white fw-bold">
+              <div class="palette__shade">Pair programming</div>
+              <div class="palette__shade">Communication</div>
+              <div class="palette__shade">Leadership</div>
+              <div class="palette__shade">Problem solving</div>
+              <div class="palette__shade">Fast learning</div>
+              <div class="palette__shade">Work ethic</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/* <div className='d-flex flex-column justify-content-center align-items-center gap-2' style={{height: '94vh', backgroundColor: '#E4D6A7', paddingTop: '5vh'}}>
       <div className={`flip-card ${isFlipped[0] ? 'flipped' : ''}`} onClick={() => handleCardClick(0)}>
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            <h2>Hard Skills</h2>
+            <h2 className='fw-bold'>Hard Skills</h2>
           </div>
           <div className="flip-card-back">
             <ul className='hard-list'>
@@ -39,7 +74,7 @@ const Skills = () => {
       <div className={`flip-card ${isFlipped[1] ? 'flipped' : ''}`} onClick={() => handleCardClick(1)}>
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            <h2>Soft Skills</h2>
+            <h2 className='fw-bold'>Soft Skills</h2>
           </div>
           <div className="flip-card-back">
             <ul className='skills-list'>
@@ -53,7 +88,8 @@ const Skills = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
+    </section>
   );
 };
 
