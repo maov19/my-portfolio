49961204967e032ad5756363e41122629b016d64
mossa <martin.ossa.8@gmail.com>
2023-08-16 11:24:03 +0200

    Update intro styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,12 +9,12 @@ const Intro = () => {
       <div className='h-50'>
         <Logo />
       </div>
-      <div className='d-flex flex-column justify-content-center align-items-center h-50' style={{backgroundColor: '#dff706bb'}}>
-        <h2>Hey, I'm Martin.</h2>
+      <div className='d-flex flex-column justify-content-center align-items-center h-50' style={{color: 'white', backgroundColor: "#0A0908"}}>
+        <h2 className='fw-bold'>Hey, I'm Martin.</h2>
         <p className='text-center w-75'>I'm a web developer. I like to create fun and interactive SPAs.</p>
         {/* <button className='w-25'>Download my resume</button> */}
         <NavLink to="/projects">
-          <button className=' p-2 GitHub-btn'>
+          <button className='see-projects-btn fw-bold p-2 my-1'>
             See my projects
           </button>
         </NavLink>
