812b4d4d47be9f1cefeac39c04a82ea1b88b578c
mossa <martin.ossa.8@gmail.com>
2023-08-16 11:24:40 +0200

    Update projects styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
       image: cycleCruiser,
       title: 'CycleCruiser',
       description: 'Web app to create, delete and make reservations to use Motorcyles.',
-      items: ['React', 'Redux', 'Rails'],
+      items: ['React', 'Rails', 'Bootstrap'],
       liveSite: 'https://thecyclecruise.onrender.com/',
       gitHub: 'https://github.com/c00p75/appointment-app-backend',
     },
@@ -28,28 +28,30 @@ const Projects = () => {
   ];
 
   return (
-    <div className='d-flex flex-column align-items-center justify-content-center gap-3 p-3' style={{ width: '100vw', backgroundColor: '#dff70675'}}>
-      <h1 style={{paddingTop: '1.5rem'}}>Projects</h1>
+    <div className='container-background'>
+    <div className='d-flex flex-column align-items-center justify-content-center gap-5 p-3 projects-background' style={{ width: '100vw', backgroundColor: '#dff70675'}}>
+      <h1 style={{paddingTop: '1.5rem', fontWeight: 'bold'}}>Projects</h1>
       {projects.map((project) => (
-        <div className='rounded w-75 h-100 d-flex flex-column justify-content-center align-items-center p-4' style={{backgroundColor: 'white'}} key={project.id}>
-          <img src={project.image} alt={project.title} className='img-fluid mb-3'/>
-          <h2 className='text-center'>{project.title}</h2>
-          <p className='text-center'>{project.description}</p>
-          <ul className='w-100 d-flex flex-row justify-content-center gap-4'>
+        <div className='w-75 h-100 d-flex flex-column justify-content-center align-items-center p-4 triple-rounded' key={project.id} style={{backgroundColor: '#22333B', color: 'white', boxShadow: '0 1.2rem 0 #0f161a'}}>
+          <img src={project.image} alt={project.title} className='border img-fluid mb-3 rounded'/>
+          <h2 className='text-center fs-4 fw-bold'>{project.title}</h2>
+          <p className='text-center' style={{fontSize: '0.8rem'}}>{project.description}</p>
+          <ul className='w-100 d-flex flex-row justify-content-center align-items-center p-0 gap-1'>
             {project.items.map((item, index) => (
-              <li key={index}>{item}</li>
+              <li className='fw-bold p-1 border-none rounded' style={{backgroundColor: '#C6AC8F', fontSize: '0.7rem', color: '#22333b'}} key={index}>{item}</li>
             ))}
           </ul>
-          <div className='d-flex flex-row gap-5'>
+          <div className='d-flex flex-row gap-4 my-1'>
             <a href={project.gitHub} target="_blank" rel="noopener noreferrer" className='text-white fs-5'>
-              <button className='custom-button GitHub-btn'>GitHub</button>
+              <button className='GitHub-btn'>GitHub</button>
             </a>
             <a href={project.liveSite} target="_blank" rel="noopener noreferrer" className='text-white fs-5'>
-              <button className='custom-button LiveSite-btn'>Live site</button>
+              <button className='LiveSite-btn'>Live site</button>
             </a>
           </div>
         </div>
       ))}
+    </div>
     </div>
   );
 };
