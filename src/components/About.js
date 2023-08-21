ade3651bc366d730e81165bb42453b2a751bd292
mossa <martin.ossa.8@gmail.com>
2023-08-21 18:03:41 +0200

    Update margins for desktop

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,9 @@ const About = () => {
   return (
     <>
     <div class="box"><b></b>
-    <section className='vh-100 vw-100 d-flex flex-column justify-content-start align-items-center text-center px-5 fw-bold about' id='about'>
-      <h1 className='mt-3 p-5 fw-bold text-center'>About Me</h1>
-      <div className='m-0 p-0 about-content fs-md-3'>
+    <section className='vh-100 vw-100 d-flex flex-column justify-content-start align-items-center text-center fw-bold about' id='about'>
+      <h1 className='fw-bold text-center'>About Me</h1>
+      <div className='about-content'>
         <p>
             Hey! I'm a motivated full stack developer with a passion for E-commerce and data analytics. 
             I have over a decade in Accounting and Finance, where I've thrived in remote, goal-oriented environments.
@@ -20,8 +20,8 @@ const About = () => {
         <p>            
             Want to know more?
         </p>
-        <a href="https://drive.google.com/file/d/1MYHBs4kdq5WNMwbLbpJeeE-PMclamAYb/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className='text-white fs-5'>
-            <button className='my-4 fw-bold fs-4 p-2 see-projects-btn'>Check my resume</button>
+        <a href="https://drive.google.com/file/d/1MYHBs4kdq5WNMwbLbpJeeE-PMclamAYb/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className='text-white'>
+            <button className='my-4 fw-bold p-2 see-projects-btn'>Check my resume</button>
         </a>
       </div>
     </section>
